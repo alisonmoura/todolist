b1a4c0c1da3b5dcd53d1c092ad3b2875abacfa09
Alison Moura <alison.oghino@gmail.com>
2016-06-20 08:41:36 -0400

    Lis of TODOS done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,36 @@
+export class Task {
+	id: number;
+	description: string;
+}
+
+const TASKS: Task[] = [
+		{id:1, description:"Some Thing"},
+		{id:2, description:"Other Thing"}
+	];
+
 import { Component } from '@angular/core'
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>My first app in angular 2</h1>'
+	templateUrl: './app/templates/todo-list.html'
 })
 
-export class AppComponent { }
+export class AppComponent { 
+
+	title = "Todo List";
+
+	public tasks = TASKS;
+	public taskToEdit = {};
+
+	selectedTask: Task;
+
+	public constructor(){
+		this.selectedTask = new Task();
+	}
+
+	showTaskDetail(task: Task){ 
+		this.selectedTask = task;
+		$('#modalShowTaskDetail').openModal();
+	};
+
+}
